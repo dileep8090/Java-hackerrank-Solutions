bc5e8c3d10a4b450a0ab2f3d597bc6e18d17a995
DILEEP KUSHWAHA <71811440+dileep8090@users.noreply.github.com>
2022-02-11 23:39:20 +0530

    Add files via upload

---
+++ 05_Java Output Formatting.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+   Scanner scan = new Scanner(System.in);
+   System.out.println("================================");
+   for(int i=0;i<3;i++){
+   String s1 = scan.next();
+   int a = scan.nextInt();
+   System.out.printf("%-15s%03d%n", s1, a);
+   
+ 
+   
+   }
+   System.out.println("================================");
+   
+   
+    }
+}
